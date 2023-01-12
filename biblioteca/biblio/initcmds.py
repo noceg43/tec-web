81389fb0fdfe877131880432137c74b4791a22df
noceg43 <noceg43@gmail.com>
2023-01-12 16:43:20 +0100

    mesi con indice 0 risolti, ora funzionante

--- biblioteca/biblio/initcmds.py
+++ biblioteca/biblio/initcmds.py
@@ -2,47 +2,51 @@ from gestione.models import Libro
 from django.utils import timezone
 from datetime import datetime
 
+
 def erase_db():
     print("Cancello il DB")
     Libro.objects.all().delete()
 
+
 def init_db():
-    
+
     if len(Libro.objects.all()) != 0:
         return
 
-    def func_time(off_year=None, off_month=None, off_day=None):
+    def func_time(off_year=0, off_month=0, off_day=0):
         tz = timezone.now()
-        out = datetime(tz.year-off_year,tz.month-off_month,
-                    tz.day-off_day,tz.hour,tz.minute, tz.second)
-        return out 
+        tz = datetime(tz.year+2, tz.month+2,
+                      tz.day+2, tz.hour, tz.minute, tz.second)
+        out = datetime(tz.year-off_year, tz.month-off_month,
+                       tz.day-off_day, tz.hour, tz.minute, tz.second)
+        return out
 
-    #se è vuoto lo inizializzo
-    #può essere letto da fonti esterne, files, altri DB etc...
+    # se è vuoto lo inizializzo
+    # può essere letto da fonti esterne, files, altri DB etc...
 
     libridict = {
-        "autori" : ["Alessandro Manzoni", "George Orwell", "Omero", "George Orwell", "Omero"],
-        "titoli" : ["Promessi Sposi", "1984", "Odissea", "La Fattoria degli Animali", "Iliade"],
-        "pagine" : [832,328,414,141,263],
-        "date" :   [ func_time(y,m,d) for y in range(2) for m in range(2) for d in range(2) ]
+        "autori": ["Alessandro Manzoni", "George Orwell", "Omero", "George Orwell", "Omero"],
+        "titoli": ["Promessi Sposi", "1984", "Odissea", "La Fattoria degli Animali", "Iliade"],
+        "pagine": [832, 328, 414, 141, 263],
+        "date":   [func_time(y, m, d) for y in range(2) for m in range(2) for d in range(2)]
     }
 
-    #for k in libridict:
+    # for k in libridict:
     #    print(str(libridict[k]))
 
     for i in range(5):
         l = Libro()
         for k in libridict:
             if k == "autori":
-                    l.autore = libridict[k][i]
+                l.autore = libridict[k][i]
             if k == "titoli":
-                    l.titolo = libridict[k][i]
+                l.titolo = libridict[k][i]
             if k == "pagine":
-                    l.pagine = libridict[k][i]
+                l.pagine = libridict[k][i]
             else:
-                l.data_prestito = libridict[k][i] 
-        #print(l)
+                l.data_prestito = libridict[k][i]
+        # print(l)
         l.save()
-    
+
     print("DUMP DB")
-    print(Libro.objects.all()) #controlliamo
+    print(Libro.objects.all())  # controlliamo
--- biblioteca/gestione/models.py
+++ biblioteca/gestione/models.py
@@ -1,18 +1,19 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
+
 class Libro(models.Model):
-    titolo = models.CharField(max_length = 200)
-    autore = models.CharField(max_length = 50)
-    pagine = models.CharField(max_length = 100)
-    data_prestito = models.DateField(default= None)
+    titolo = models.CharField(max_length=200)
+    autore = models.CharField(max_length=50)
+    pagine = models.CharField(max_length=100)
+    data_prestito = models.DateField(default=None)
 
     def __str__(self):
         out = self.titolo + " di " + self.autore
         if self.data_prestito == None:
-            out += "attualmente non in prestito"
+            out += " attualmente non in prestito"
         else:
-            out += "in prestito dal " + str(self.data_prestito)
+            out += " in prestito dal " + str(self.data_prestito)
         return out
 
     class Meta:
